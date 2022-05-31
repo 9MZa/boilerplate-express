0fbd864ad79b6eea2f3bed3bff787adb08814c2e
Bandit Silachai <mbandit001@yahoo.co.th>
2022-05-31 21:10:10 +0700

    add middleware

--- myApp.js
+++ myApp.js
@@ -7,19 +7,23 @@ let absolutePath = __dirname + "/views/index.html";
 app.use(express.static('public'));
 app.use('/public', express.static('public'))
 
+app.use((req, res, next) => {
+  console.log("middleware");
+  next();
+})
 
 app.get('/json', (req, res) => {
   if (process.env.MESSAGE_STYLE === "uppercase") {
-    res.json({"message": "HELLO JSON"})
+    res.json({ "message": "HELLO JSON" })
   } else {
-    res.json({"message": "Hello json"})
+    res.json({ "message": "Hello json" })
   }
 });
 
 
 
 app.get('/', (req, res) => {
-    res.sendFile(absolutePath);
+  res.sendFile(absolutePath);
 });
 
 console.log('Hello World');
