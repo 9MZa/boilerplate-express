57ebafecebd7091acb396951aa1e43851e3369cf
Bandit Silachai <mbandit001@yahoo.co.th>
2022-05-31 21:22:51 +0700

    Logger Middleware

--- myApp.js
+++ myApp.js
@@ -7,10 +7,12 @@ let absolutePath = __dirname + "/views/index.html";
 app.use(express.static('public'));
 app.use('/public', express.static('public'))
 
-app.use((req, res, next) => {
-  console.log("middleware");
+const Logger = (req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
-})
+}
+
+app.use(Logger)
 
 app.get('/json', (req, res) => {
   if (process.env.MESSAGE_STYLE === "uppercase") {
